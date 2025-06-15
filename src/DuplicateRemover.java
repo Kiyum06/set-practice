e8742e86427f0fceb1b5a7e3d0f296f1b17f3a85
Kiyum-06 <kiyum1406@gmail.com>
2025-06-15 12:08:20 -0700

    Completed the DuplicateRemover.

--- src/DuplicateRemover.java
+++ src/DuplicateRemover.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * The DuplicateRemover class provides a method to remove duplicate words
@@ -7,19 +10,25 @@ import java.util.List;
 public class DuplicateRemover {
 
     /**
-     * Removes duplicate words from the input array, sorts them in lexicographic order,
+     * Removes duplicate words from the input array, sorts them in lexicographic
+     * order,
      * and returns a list of unique words.
      *
      * @param words an array of strings that may contain duplicate words
      * @return a sorted List<String> containing unique words from the input array
      */
     public static List<String> sortAndRemoveDuplicates(String[] words) {
-        // TODO
-        return null;
+        Set<String> sortedSet = new TreeSet<>();
+
+        for (String word : words) {
+            sortedSet.add(word);
+        }
+
+        return new ArrayList<>(sortedSet);
     }
 
     public static void main(String[] args) {
-        String[] words = {"yes", "no", "maybe", "yes", "yes"};
+        String[] words = { "yes", "no", "maybe", "yes", "yes" };
         List<String> uniqueWords = sortAndRemoveDuplicates(words);
         System.out.println(uniqueWords);
     }
